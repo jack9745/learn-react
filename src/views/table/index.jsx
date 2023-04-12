e9cad62ac064cfb9f2496350f63cc8616c7d103a
huangtao <120473417@qq.com>
2023-04-12 21:15:39 +0800

    添加打印信息

--- src/views/table/index.jsx
+++ src/views/table/index.jsx
@@ -5,6 +5,9 @@ import React from 'react'
 import { Button, Table } from 'antd'
 export default function Index() {
   const handleClick = () => {
+    console.log(
+      '我加了一个main分支的打印信息，dev分支没有这条打印信息，测试从dev分支拉取到main分支会不会被删除'
+    )
     console.log('这是一个按钮')
   }
   return (
