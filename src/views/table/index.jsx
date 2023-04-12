ccb0d88a857e638e06a3aaf53040968c28dbcaad
huangtao <120473417@qq.com>
2023-04-12 21:23:41 +0800

    测试cherry-pick 解决冲突提交

--- src/views/table/index.jsx
+++ src/views/table/index.jsx
@@ -8,6 +8,11 @@ console.log('删除了第二行的数据')
 import { Button, Table } from 'antd'
 export default function Index() {
   const handleClick = () => {
+    console.log(
+      '我加了一个main分支的打印信息，dev分支没有这条打印信息，测试从dev分支拉取到main分支会不会被删除'
+    )
+
+    console.log('经过测试好像不会被删除')
     console.log('这是一个按钮')
   }
   return (
